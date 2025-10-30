de88854cc91b47c3902ba8466db7e43497a0c39b
Titiss77 <Mathis.fcs.lav@gmail.com>
2025-10-30 22:25:00 +0100

    new

---
+++ config.php
@@ -0,0 +1,1 @@
+# Ici se trouve les informations de connexion à la base de données MySQL mais pour une raison évidente elles ne seront pas données.
--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require 'serveur.php';
+require_once __DIR__ . '/serveur.php';
 
 
 // Récupérer les infos contact (1 seul enregistrement)
